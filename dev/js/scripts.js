2ed702309479763352cf9ca7037ec926466a61e1
JulesSC <81724365+JulesSC@users.noreply.github.com>
2021-10-03 20:14:09 -0400

    pretty much got my animation in place

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -2,4 +2,6 @@ import { gsap } from "gsap";
 
 const mainTL = gsap.timeline();
 
-mainTL.from("content",{duration:2, alpha:0, y:"+=1000", scale:2});
+mainTL.from("#hero",{duration:2, alpha:0});
+mainTL.from("#hero >div",{duration:0.5, alpha:0}, "madeUp");
+mainTL.from("#hero >div section",{duration:0.5, rotation: 180}, "madeUp");
