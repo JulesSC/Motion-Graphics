e4aab9123ede96681b66bd697d39ec80442f74ae
JulesSC <81724365+JulesSC@users.noreply.github.com>
2021-10-04 11:33:56 -0400

    justtt messing around

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -4,31 +4,31 @@ const mainTL = gsap.timeline();
 
 mainTL.from("#hero",{duration:2, alpha:0});
 mainTL.from("#hero >div",{duration:0.5, alpha:0}, "madeUp");
-// mainTL.from("#hero >div section",{duration:0.5, rotation: 180}, "madeUp");
+mainTL.from("#hero >div section",{duration:0.5, rotation: 180}, "madeUp");
 
-gsap.set("#hero >div section", {xPercent: -50, yPercent: -50});
+// gsap.set("#hero >div section", {xPercent: -50, yPercent: -50});
 
-const ball = document.querySelector("#hero >div section");
-const pos = { x: window.innerWidth / 2, y: window.innerHeight / 2 };
-const mouse = { x: pos.x, y: pos.y };
-const speed = 0.35;
+// var ball = document.querySelector("#hero >div section");
+// var pos = { x: window.innerWidth / 2, y: window.innerHeight / 2 };
+// var mouse = { x: pos.x, y: pos.y };
+// var speed = 0.35;
 
-const xSet = gsap.quickSetter(ball, "x", "px");
-const ySet = gsap.quickSetter(ball, "y", "px");
+// var xSet = gsap.quickSetter(ball, "x", "px");
+// var ySet = gsap.quickSetter(ball, "y", "px");
 
-window.addEventListener("mousemove", e => {    
-  mouse.x = e.x;
-  mouse.y = e.y;  
-});
+// window.addEventListener("mousemove", e => {    
+//   mouse.x = e.x;
+//   mouse.y = e.y;  
+// });
 
-gsap.ticker.add(() => {
+// gsap.ticker.add(() => {
   
-  // adjust speed for higher refresh monitors
-  const dt = 1.0 - Math.pow(1.0 - speed, gsap.ticker.deltaRatio()); 
+//   // adjust speed for higher refresh monitors
+//   var dt = 1.0 - Math.pow(1.0 - speed, gsap.ticker.deltaRatio()); 
   
-  pos.x += (mouse.x - pos.x) * dt;
-  pos.y += (mouse.y - pos.y) * dt;
-  xSet(pos.x);
-  ySet(pos.y);
-});
+//   pos.x += (mouse.x - pos.x) * dt;
+//   pos.y += (mouse.y - pos.y) * dt;
+//   xSet(pos.x);
+//   ySet(pos.y);
+// });
 
