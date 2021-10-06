6d6268e149e2af450e9e3c84809bd4cc5bc6d521
JulesSC <81724365+JulesSC@users.noreply.github.com>
2021-10-06 09:13:22 -0400

    few slide ins

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -15,10 +15,11 @@ const mainTL = gsap.timeline();
 
 
 mainTL.from("#hero",{duration:1, y:"+=100"}, "madeUp");
-mainTL.from("header",{duration:1, y:"+=100"}, "madeUp");
-mainTL.from("header",{duration:0.7, alpha:0}, "madeUp");
+mainTL.from("header",{duration:1, alpha:0, y:"+=100"}, "madeUp");
+// mainTL.from("header",{duration:0.7, alpha:0}, "madeUp");
 mainTL.from("#content",{duration:1, y:"+=100"}, "madeUp");
-mainTL.from("#hero >div",{duration:0.5, alpha:0}, "madeUP");
+mainTL.from("header ul",{duration:0.5, alpha:0, y:"+=10", delay: 0.2}, "madeUP");
+mainTL.from("#hero >div",{duration:0.5, alpha:0, y:"+=5", delay: 0.2}, "madeUP");
 mainTL.from("#hero >div section",{duration:0.5, rotation: 180}, "madeUP");
 
 
