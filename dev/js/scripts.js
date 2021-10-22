27bc585c65db1f34ff60eef5f395737ff4365c59
JulesSC <81724365+JulesSC@users.noreply.github.com>
2021-10-22 12:11:14 -0400

    things are happenin

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -2,28 +2,43 @@ import { gsap } from "gsap";
 
 const mainTL = gsap.timeline();
 
-// mainTL.to("#hero >div section",{duration:1, alpha:1, y:"-=100"});
-// mainTL.to("#hero >div section",{duration:0.5, rotation: 180});
-// mainTL.to("#hero >div section",{duration:1, alpha:0, y:"+=100"});
+// mainTL.to("#Left-Ellipse", {duration: 1, alpha: 0, scale:2});
 
-// mainTL.from("#hero",{duration:1, y:"+=100", delay: 2.5}, "madeUp");
-// mainTL.from("#hero",{duration:0.7, alpha:0, delay: 2.5}, "madeUp");
-// mainTL.from("header",{duration:1, y:"+=100", delay: 2.5}, "madeUp");
-// mainTL.from("header",{duration:0.7, alpha:0, delay: 2.5}, "madeUp");
-// mainTL.from("#content",{duration:1, y:"+=100", delay: 2.5}, "madeUp");
-// mainTL.from("#hero-body",{duration:0.5, alpha:0}, "madeUP");
+function morphEllipseLeft(){
+    const tl = gsap.timeline();
+    tl.to("#Left-Ellipse", {duration: 2, y:"-=500"});
+    return tl;
+}
 
+function morphEllipseRight(){
+    const tl = gsap.timeline();
+    tl.to("#Right-Ellipse", {duration: 2, y:"+=500"});
+    return tl;
+}
 
-mainTL.from("#hero",{duration:1, y:"+=100"}, "madeUp");
-mainTL.from("header",{duration:1, alpha:0, y:"+=100"}, "madeUp");
-// mainTL.from("header",{duration:0.7, alpha:0}, "madeUp");
-mainTL.from("#content",{duration:1, y:"+=100"}, "madeUp");
-mainTL.from("header ul",{duration:0.5, alpha:0, y:"+=10", delay: 0.2}, "madeUP");
-mainTL.from("#hero >div",{duration:0.5, alpha:0, y:"+=5", delay: 0.2}, "madeUP");
-mainTL.from("#hero >div section",{duration:0.5, rotation: 180}, "madeUP");
+function flipWaveLower(){
+    const tl = gsap.timeline();
+    tl.to("#Group-1, #Group-3, #Group-5", {duration: 2, scaleY:-1});
+    return tl;
+}
 
+// function flipWaveUpper(){
+//     const tl = gsap.timeline();
+//     tl.from("#Group-2, #Group-4, #Group-6", {duration: 2, scaleY:-1});
+//     return tl;
+// }
 
-// gsap.from("#content", {
-//     scrollTrigger: "#content", duration:0.5, alpha:0 // start the animation when ".box" enters the viewport (once)
-//     // y:"+=100"
-//   });
+mainTL.add(morphEllipseLeft(), "morph")
+.add(morphEllipseRight(), "morph")
+.add(flipWaveLower()), "morph";
+
+/* =============
+    Hero
+============= */
+
+// mainTL.from("#hero",{duration:1, y:"+=100"}, "madeUp");
+// mainTL.from("header",{duration:1, alpha:0, y:"+=100"}, "madeUp");
+// mainTL.from("#content",{duration:1, y:"+=100"}, "madeUp");
+// mainTL.from("header ul",{duration:0.5, alpha:0, y:"+=10", delay: 0.2}, "madeUP");
+// mainTL.from("#hero >div",{duration:0.5, alpha:0, y:"+=5", delay: 0.2}, "madeUP");
+// mainTL.from("#hero >div section",{duration:0.5, rotation: 180}, "madeUP");
